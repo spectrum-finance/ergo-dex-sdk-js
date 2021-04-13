36fd99ddedd6d79cab8980e77043ce94e49b15c5
oskin1 <ilya.arcadich@gmail.com>
2021-04-13 12:16:46 +0300

    Outputs aggregation.

--- src/wallet/entities/boxSelection.ts
+++ src/wallet/entities/boxSelection.ts
@@ -2,13 +2,15 @@ import {
     BoxSelection as LibBoxSelection,
     ErgoBoxes,
     ErgoBoxAssetsData,
-    BoxValue, I64, Tokens, Token, TokenAmount, ErgoBoxAssetsDataList
+    BoxValue, I64, Tokens, Token as LibToken, TokenAmount, ErgoBoxAssetsDataList, TokenId as LibTokenId
 } from "ergo-lib-wasm-browser";
 import {ChangeBox} from "./changeBox";
 import {EmptyInputs} from "../errors/emptyInputs";
 import {OverallAmount} from "./overallAmount";
 import {notImplemented} from "../../utils/notImplemented";
 import {ErgoBox} from "./ergoBox";
+import {Token} from "./token";
+import {TokenId} from "../types";
 
 export class BoxSelection {
     readonly boxes: ErgoBox[]
@@ -30,7 +32,17 @@ export class BoxSelection {
     /** Amounts of all kinds of tokens with change excluded.
      */
     totalOutputWithoutChange(): OverallAmount {
-        return notImplemented()
+        let nErgsIn = this.boxes.map((bx, _i, _xs) => bx.value).reduce((x, y, _i, _xs) => x + y)
+        let nErgsChange = this.change?.value || 0n
+        let nErgs = nErgsIn - nErgsChange
+        let tokensAgg = new Map<TokenId, bigint>()
+        for (let t of this.boxes.flatMap((bx, _i, _xs) => bx.tokens)) {
+            let acc = tokensAgg.get(t.id) || 0n
+            tokensAgg.set(t.id, t.amount + acc)
+        }
+        let tokens: Token[] = []
+        tokensAgg.forEach((amount, id, _xs) => tokens.push(new Token(id, amount)))
+        return { nErgs: nErgs, tokens: tokens }
     }
 
     toErgoLib(): LibBoxSelection {
@@ -39,7 +51,7 @@ export class BoxSelection {
         let changeList = new ErgoBoxAssetsDataList()
         if (this.change) {
             this.change.tokens.forEach((v, k, _xs) => {
-                tokens.add(new Token(k, TokenAmount.from_i64(I64.from_str(v.toString()))))
+                tokens.add(new LibToken(k, TokenAmount.from_i64(I64.from_str(v.toString()))))
             })
             let change = new ErgoBoxAssetsData(BoxValue.from_i64(I64.from_str(this.change.value.toString())), tokens)
             for (let box of this.boxes.slice(1)) boxes.add(box.toErgoLib())
