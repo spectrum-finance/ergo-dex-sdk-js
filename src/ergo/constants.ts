e805bd3c22055544907185ba2bce32293cdbc406
oskin1 <ilya.arcadich@gmail.com>
2021-05-28 23:16:07 +0300

    Static miner fee address.

--- src/ergo/constants.ts
+++ src/ergo/constants.ts
@@ -1,11 +1,9 @@
-import { RustModule } from '../utils/rustLoader';
-
 import {Address} from "./entities/address";
 
 export const MinBoxAmountNErgs = 10000n
 
-// TODO: We should use functions to get values, because constants initialise
-//  at the module start.
-//  need to RustModule.load() first
-export const MinerAddressMainnet: Address = RustModule.SigmaRust.MinerAddress.mainnet_fee_address()
-export const MinerAddressTestnet: Address = RustModule.SigmaRust.MinerAddress.testnet_fee_address()
+export const MinerAddressMainnet: Address =
+    "2iHkR7CWvD1R4j1yZg5bkeDRQavjAaVPeTDFGGLZduHyfWMuYpmhHocX8GJoaieTx78FntzJbCBVL6rf96ocJoZdmWBL2fci7NqWgA" +
+    "irppPQmZ7fN9V6z13Ay6brPriBKYqLp1bT2Fk4FkFLCfdPpe"
+
+export const MinerAddressTestnet: Address = ""
