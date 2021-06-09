c001a03ec2e6cd35def521f441ffc19a77ac85fb
oskin1 <ilya.arcadich@gmail.com>
2021-06-09 17:20:27 +0300

    Transactions service extended.

--- src/ergo/wallet/services/transactions.ts
+++ src/ergo/wallet/services/transactions.ts
@@ -1,23 +1,25 @@
-import {Address} from "../../entities/address"
-import {TransactionContext} from "../entities/transactionContext"
-import {ErgoTx} from "../../entities/ergoTx"
-import {Prover} from "../prover"
-import {TxAssembler} from "../txAssembler"
-import {ergoTreeFromAddress} from "../../entities/ergoTree"
+import { Address } from "../../entities/address"
+import { TransactionContext } from "../entities/transactionContext"
+import { ErgoTx } from "../../entities/ergoTx"
+import { Prover } from "../prover"
+import { TxAssembler } from "../txAssembler"
+import { ergoTreeFromAddress } from "../../entities/ergoTree"
+import { TokenAmount } from "../../entities/tokenAmount"
 
 export interface Transactions {
-  simple(amount: bigint, recipient: Address, ctx: TransactionContext): Promise<ErgoTx>
+  simple(amount: bigint, tokens: TokenAmount[], recipient: Address, ctx: TransactionContext): Promise<ErgoTx>
 }
 
 export class DefaultTransactions implements Transactions {
-  constructor(public readonly prover: Prover, public readonly txAsm: TxAssembler) {}
+  constructor(public readonly prover: Prover, public readonly txAsm: TxAssembler) {
+  }
 
-  async simple(amount: bigint, recipient: Address, ctx: TransactionContext): Promise<ErgoTx> {
+  async simple(amount: bigint, assets: TokenAmount[], recipient: Address, ctx: TransactionContext): Promise<ErgoTx> {
     const out = {
       value: Number(amount),
       ergoTree: ergoTreeFromAddress(recipient),
       creationHeight: ctx.network.height,
-      assets: [],
+      assets: assets,
       additionalRegisters: {}
     }
     const req = {
