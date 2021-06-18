0a72bb74b6e9c7f0baf312da1d42f2a609de1e57
oskin1 <ilya.arcadich@gmail.com>
2021-06-18 20:22:57 +0300

    WIP.

--- src/amm/contracts/t2tPoolContracts.ts
+++ src/amm/contracts/t2tPoolContracts.ts
@@ -1,9 +1,9 @@
-import {PoolId} from "../types"
+import { PoolId } from "../types"
 import * as templates from "./templates"
-import {NErg, TokenId, ErgoTree, PublicKey} from "../../ergo"
-import {fromHex} from "../../utils/hex"
-import {RustModule} from "../../utils/rustLoader"
-import {SigmaPropConstPrefixHex} from "../constants"
+import { NErg, TokenId, ErgoTree, PublicKey } from "../../ergo"
+import { fromHex } from "../../utils/hex"
+import { RustModule } from "../../utils/rustLoader"
+import { SigmaPropConstPrefixHex } from "../constants"
 
 export class T2tPoolContracts {
   static pool(): ErgoTree {
@@ -43,17 +43,17 @@ export class T2tPoolContracts {
     let tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tSwap)
     tree.set_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
     tree.set_constant(3, RustModule.SigmaRust.Constant.from_byte_array(fromHex(quoteId)))
-    tree.set_constant(10, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
+    tree.set_constant(8, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
+    tree.set_constant(9, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
+    tree.set_constant(11, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
     tree.set_constant(
-      12,
+      13,
       RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(minQuoteAmount.toString()))
     )
     tree.set_constant(
-      13,
+      14,
       RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFeePerToken.toString()))
     )
-    tree.set_constant(14, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
-    tree.set_constant(15, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
     return tree.to_base16_bytes()
   }
 }
