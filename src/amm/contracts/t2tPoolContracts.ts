4f4fbb4e4ae2e5aa30565180abd7e7436541eef5
oskin1 <ilya.arcadich@gmail.com>
2021-06-23 20:34:56 +0300

    SigmaWasm updated.

--- src/amm/contracts/t2tPoolContracts.ts
+++ src/amm/contracts/t2tPoolContracts.ts
@@ -1,9 +1,9 @@
-import { PoolId } from "../types"
+import {PoolId} from "../types"
 import * as templates from "./templates"
-import { NErg, TokenId, ErgoTree, PublicKey } from "../../ergo"
-import { fromHex } from "../../utils/hex"
-import { RustModule } from "../../utils/rustLoader"
-import { SigmaPropConstPrefixHex } from "../constants"
+import {NErg, TokenId, ErgoTree, PublicKey} from "../../ergo"
+import {fromHex} from "../../utils/hex"
+import {RustModule} from "../../utils/rustLoader"
+import {SigmaPropConstPrefixHex} from "../constants"
 
 export class T2tPoolContracts {
   static pool(): ErgoTree {
@@ -11,25 +11,25 @@ export class T2tPoolContracts {
   }
 
   static deposit(poolId: PoolId, pk: PublicKey, dexFee: bigint): ErgoTree {
-    let tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tDeposit)
-    tree.set_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
-    tree.set_constant(8, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
-    tree.set_constant(
-      10,
-      RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFee.toString()))
-    )
-    return tree.to_base16_bytes()
+    return RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tDeposit)
+      .with_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
+      .with_constant(8, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
+      .with_constant(
+        10,
+        RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFee.toString()))
+      )
+      .to_base16_bytes()
   }
 
   static redeem(poolId: PoolId, pk: PublicKey, dexFee: bigint): ErgoTree {
-    let tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tRedeem)
-    tree.set_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
-    tree.set_constant(10, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
-    tree.set_constant(
-      12,
-      RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFee.toString()))
-    )
-    return tree.to_base16_bytes()
+    return RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tRedeem)
+      .with_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
+      .with_constant(10, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
+      .with_constant(
+        12,
+        RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFee.toString()))
+      )
+      .to_base16_bytes()
   }
 
   static swap(
@@ -40,20 +40,20 @@ export class T2tPoolContracts {
     dexFeePerToken: NErg,
     pk: PublicKey
   ): ErgoTree {
-    let tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tSwap)
-    tree.set_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
-    tree.set_constant(3, RustModule.SigmaRust.Constant.from_byte_array(fromHex(quoteId)))
-    tree.set_constant(8, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
-    tree.set_constant(9, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
-    tree.set_constant(11, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
-    tree.set_constant(
-      13,
-      RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(minQuoteAmount.toString()))
-    )
-    tree.set_constant(
-      14,
-      RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFeePerToken.toString()))
-    )
-    return tree.to_base16_bytes()
+    return RustModule.SigmaRust.ErgoTree.from_base16_bytes(templates.T2tSwap)
+      .with_constant(0, RustModule.SigmaRust.Constant.decode_from_base16(SigmaPropConstPrefixHex + pk))
+      .with_constant(3, RustModule.SigmaRust.Constant.from_byte_array(fromHex(quoteId)))
+      .with_constant(8, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
+      .with_constant(9, RustModule.SigmaRust.Constant.from_i32(poolFeeNum))
+      .with_constant(11, RustModule.SigmaRust.Constant.from_byte_array(fromHex(poolId)))
+      .with_constant(
+        13,
+        RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(minQuoteAmount.toString()))
+      )
+      .with_constant(
+        14,
+        RustModule.SigmaRust.Constant.from_i64(RustModule.SigmaRust.I64.from_str(dexFeePerToken.toString()))
+      )
+      .to_base16_bytes()
   }
 }
--- src/amm/entities/ammPool.ts
+++ src/amm/entities/ammPool.ts
@@ -3,73 +3,83 @@ import {AssetInfo, AssetAmount, HexString} from "../../ergo"
 import {Price} from "../../entities/price"
 
 export class AmmPool {
-    constructor(
-        public readonly id: PoolId,
-        public readonly x: AssetAmount,
-        public readonly y: AssetAmount,
-        public readonly poolScriptHash: HexString,
-        public readonly poolFeeNum: number
-    ) {
-    }
+  constructor(
+    public readonly id: PoolId,
+    public readonly x: AssetAmount,
+    public readonly y: AssetAmount,
+    public readonly poolScriptHash: HexString,
+    public readonly poolFeeNum: number
+  ) {}
 
-    private feeDenom: bigint = 1000n
-    private feeNum: bigint = BigInt(this.poolFeeNum)
+  private feeDenom: bigint = 1000n
+  private feeNum: bigint = BigInt(this.poolFeeNum)
 
-    get assetX(): AssetInfo {
-        return this.x.asset
-    }
+  get assetX(): AssetInfo {
+    return this.x.asset
+  }
 
-    get assetY(): AssetInfo {
-        return this.y.asset
-    }
+  get assetY(): AssetInfo {
+    return this.y.asset
+  }
 
-    /** @return Price of tokenX in tokenY units.
-     */
-    get priceX(): Price {
-        return new Price(this.y.amount, this.x.amount)
-    }
+  /** @return Price of tokenX in tokenY units.
+   */
+  get priceX(): Price {
+    return new Price(this.y.amount, this.x.amount)
+  }
 
-    /** @return Price of tokenY in tokenX units.
-     */
-    get priceY(): Price {
-        return new Price(this.x.amount, this.y.amount)
-    }
+  /** @return Price of tokenY in tokenX units.
+   */
+  get priceY(): Price {
+    return new Price(this.x.amount, this.y.amount)
+  }
 
-    /** @return Proportional amount of one token to a given input of the other
-     */
-    depositAmount(input: AssetAmount): AssetAmount {
-        if (input.asset === this.assetX)
-            return this.y.withAmount(input.amount * this.priceX.numerator / this.priceX.denominator)
-        else
-            return this.x.withAmount(input.amount * this.priceY.numerator / this.priceY.denominator)
-    }
+  /** @return Proportional amount of one token to a given input of the other
+   */
+  depositAmount(input: AssetAmount): AssetAmount {
+    if (input.asset === this.assetX)
+      return this.y.withAmount((input.amount * this.priceX.numerator) / this.priceX.denominator)
+    else return this.x.withAmount((input.amount * this.priceY.numerator) / this.priceY.denominator)
+  }
 
-    /** @return Input amount of one token for a given output amount of the other
-     */
-    inputAmount(output: AssetAmount, maxSlippage?: number): AssetAmount | undefined {
-        let slippage = BigInt(maxSlippage || 0)
-        let minimalOutput = this.outputAmount(output).amount
-        if (output.asset === this.assetX && minimalOutput > 0) {
-            return this.y.withAmount(this.y.amount * output.amount * this.feeDenom /
-                ((this.x.amount + this.x.amount * slippage / 100n - output.amount) * this.feeNum) + 1n)
-        } else if (output.asset === this.assetY && minimalOutput > 0) {
-            return this.x.withAmount(this.x.amount * output.amount * this.feeDenom /
-                ((this.y.amount + this.y.amount * slippage / 100n - output.amount) * this.feeNum) + 1n)
-        } else {
-            return undefined
-        }
+  /** @return Input amount of one token for a given output amount of the other
+   */
+  inputAmount(output: AssetAmount, maxSlippage?: number): AssetAmount | undefined {
+    let slippage = BigInt(maxSlippage || 0)
+    let minimalOutput = this.outputAmount(output).amount
+    if (output.asset === this.assetX && minimalOutput > 0) {
+      return this.y.withAmount(
+        (this.y.amount * output.amount * this.feeDenom) /
+          ((this.x.amount + (this.x.amount * slippage) / 100n - output.amount) * this.feeNum) +
+          1n
+      )
+    } else if (output.asset === this.assetY && minimalOutput > 0) {
+      return this.x.withAmount(
+        (this.x.amount * output.amount * this.feeDenom) /
+          ((this.y.amount + (this.y.amount * slippage) / 100n - output.amount) * this.feeNum) +
+          1n
+      )
+    } else {
+      return undefined
     }
+  }
 
-    /** @return Output amount of one token for a given input amount of the other
-     */
-    outputAmount(input: AssetAmount, maxSlippage?: number): AssetAmount {
-        let slippage = BigInt(maxSlippage || 0)
-        if (input.asset === this.assetX) {
-            return this.y.withAmount(this.y.amount * input.amount * this.feeNum /
-                (this.x.amount + this.x.amount * slippage / 100n) * this.feeDenom + input.amount * this.feeNum)
-        } else {
-            return this.x.withAmount(this.x.amount * input.amount * this.feeNum /
-                (this.y.amount + this.y.amount * slippage / 100n) * this.feeDenom + input.amount * this.feeNum)
-        }
+  /** @return Output amount of one token for a given input amount of the other
+   */
+  outputAmount(input: AssetAmount, maxSlippage?: number): AssetAmount {
+    let slippage = BigInt(maxSlippage || 0)
+    if (input.asset === this.assetX) {
+      return this.y.withAmount(
+        ((this.y.amount * input.amount * this.feeNum) / (this.x.amount + (this.x.amount * slippage) / 100n)) *
+          this.feeDenom +
+          input.amount * this.feeNum
+      )
+    } else {
+      return this.x.withAmount(
+        ((this.x.amount * input.amount * this.feeNum) / (this.y.amount + (this.y.amount * slippage) / 100n)) *
+          this.feeDenom +
+          input.amount * this.feeNum
+      )
     }
+  }
 }
