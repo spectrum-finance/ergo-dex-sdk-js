46621a66b397da9eb331f4f7eba0efec03b6c13e
yashablack <iamyashablack@gmail.com>
2022-01-15 14:37:22 +0300

    feat: add millis to blocks function

--- src/utils/blocks.ts
+++ src/utils/blocks.ts
@@ -1,5 +1,9 @@
 import {BlocktimeMillis} from "../constants"
 
-export function blocksToMillisEstimate(blocks: number): bigint {
+export function blocksToMillis(blocks: number): bigint {
   return BigInt(blocks) * BlocktimeMillis
+}
+
+export function millisToBlocks(millis: bigint): number {
+  return Number(millis / BlocktimeMillis)
 }
