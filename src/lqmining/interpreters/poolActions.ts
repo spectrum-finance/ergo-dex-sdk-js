2b4d4d1e92b6dfa965be079c03f8c6307f132b20
rsalakhov <r.salahov@spectrumlabs.fi>
2022-12-09 17:32:56 +0300

    update version to 1.8.5 & fix deposit operation

--- src/lqmining/interpreters/poolActions.ts
+++ src/lqmining/interpreters/poolActions.ts
@@ -71,7 +71,7 @@ class LmPoolActions implements PoolActions<TxRequest> {
       additionalRegisters: EmptyRegisters
     }
     const uiRewardOut: ErgoBoxCandidate[] = this.mkUiReward(ctx.network.height, ctx.uiFee)
-    const inputs = await this.selector.select({nErgs: ctx.minBoxValue + ctx.uiFee, assets: [depositInput]})
+    const inputs = await this.selector.select({nErgs: ctx.minBoxValue + ctx.uiFee + ctx.minerFee, assets: [depositInput]})
     if (inputs instanceof BoxSelection) {
       return {
         inputs: inputs,
