3e2aec97b20427136fd163d1012401cbfcd6db83
oskin1 <ilya.arcadich@gmail.com>
2023-01-19 16:53:07 +0300

    LM protocol updated.

--- src/lqmining/models/stake.ts
+++ src/lqmining/models/stake.ts
@@ -1,9 +1,8 @@
-import {AssetAmount, TokenId} from "@ergolabs/ergo-sdk"
+import {AssetAmount} from "@ergolabs/ergo-sdk"
 import {PoolId} from "../types"
 
 export type Stake = {
   readonly poolId: PoolId
-  readonly redeemerKeyId: TokenId
   readonly lockedLq: AssetAmount
   readonly bundleKeyAsset: AssetAmount
 }
