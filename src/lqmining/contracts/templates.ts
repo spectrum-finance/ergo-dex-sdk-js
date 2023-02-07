9f45c4c7be97ca8ae6de3da1ac0d75a379cb9ba3
oskin1 <ilya.arcadich@gmail.com>
2023-02-07 16:41:59 +0300

    Update LM protocol once again.

--- src/lqmining/contracts/templates.ts
+++ src/lqmining/contracts/templates.ts
@@ -1,7 +1,7 @@
 import {HexString} from "@ergolabs/ergo-sdk"
 
 export const StakingBundleTreeBlake2b256: HexString =
-  "a20a53f905f41ebdd71c2c239f270392d0ae0f23f6bd9f3687d166eea745bbf6"
+  "fc3cdbfd1abc83f4a38ca3fb3dfe417a158b67d63e3c52137fdda4e66ad3956c"
 
 export const StakingBundleTemplateHash: HexString =
-  "545a7f321d0c76136b4431abfe450c3c283f1e207baa6e3dce4a8c7330057b77"
+  "99ab8a13a9e84a4fd50ef4aff52b7f7807f38b993ce7b880cbd8bff20e21ca14"
