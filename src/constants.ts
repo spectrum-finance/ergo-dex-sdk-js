ba8ec6e89c57acd8defbca50a870491aba8c6979
rsalakhov <r.salahov@spectrumlabs.fi>
2023-02-13 15:52:42 +0300

    update version and spec token id

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const NativeAssetTicker = "ERG"
 export const NativeAssetDecimals = 9
 export const BlocktimeMillis = BigInt(2 * 60 * 1000)
 
-export const SpecAssetId = "ef802b475c06189fdbf844153cdc1d449a5ba87cce13d11bb47b5a539f27f12b"
+export const SpecAssetId = "003bd19d0187117f130b62e1bcab0939929ff5c7709f843c5c4dd158949285d0"
