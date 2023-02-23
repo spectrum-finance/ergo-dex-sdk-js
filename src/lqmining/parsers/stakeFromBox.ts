c0463abf3b75bc04a6b06996364977d3d2da6f83
rsalakhov <r.salahov@spectrumlabs.fi>
2023-02-23 18:44:01 +0300

    update stake parser

--- src/lqmining/parsers/stakeFromBox.ts
+++ src/lqmining/parsers/stakeFromBox.ts
@@ -6,14 +6,28 @@ import {Stake} from "../models/stake"
 export class StakeFromBox implements FromBox<Stake> {
   from(box: ErgoBox): Stake | undefined {
     const r5 = box.additionalRegisters[RegisterId.R5]
-    if (box.assets.length == 3 && r5) {
-      const poolId = deserializeConstant(r5)
-      if (poolId instanceof ByteaConstant) {
-        return {
-          poolId: toHex(poolId.value),
-          lockedLq: AssetAmount.fromToken(box.assets[0]),
-          bundleKeyAsset: AssetAmount.fromToken(box.assets[2])
-        }
+
+    if (!r5) {
+      return undefined;
+    }
+
+    const poolId = deserializeConstant(r5);
+    if (!(poolId instanceof ByteaConstant)) {
+      return undefined;
+    }
+
+    if (box.assets.length === 3) {
+      return {
+        poolId: toHex(poolId.value),
+        lockedLq: AssetAmount.fromToken(box.assets[0]),
+        bundleKeyAsset: AssetAmount.fromToken(box.assets[2])
+      }
+    }
+    if (box.assets.length === 2) {
+      return {
+        poolId: toHex(poolId.value),
+        lockedLq: AssetAmount.fromToken(box.assets[0]),
+        bundleKeyAsset: AssetAmount.fromToken(box.assets[2])
       }
     }
     return undefined
