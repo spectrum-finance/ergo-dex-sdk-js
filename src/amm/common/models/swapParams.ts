3368f1febe357eb32c5acf46c4c1f9d746b79747
rsalakhov <r.salahov@spectrumlabs.fi>
2023-02-25 14:03:48 +0300

    add make target util

--- src/amm/common/models/swapParams.ts
+++ src/amm/common/models/swapParams.ts
@@ -1,4 +1,5 @@
 import {AssetAmount, PublicKey, TokenId} from "@ergolabs/ergo-sdk"
+import {NetworkContext} from "@ergolabs/ergo-sdk/build/main/entities/networkContext"
 import {ExFee, ExFeePerToken, ExFeeType} from "../../../types"
 import {PoolId} from "../types"
 
@@ -12,4 +13,15 @@ export type SwapParams<F extends ExFeeType> = {
   readonly exFeePerToken: ExFeePerToken<F>
   readonly maxExFee: ExFee<F>;
   readonly uiFee: bigint
+}
+
+export interface SwapParamsV2 {
+  readonly address: string;
+  readonly pk: PublicKey;
+  readonly baseInput: AssetAmount;
+  readonly slippage: number;
+  readonly nitro: number;
+  readonly minExFee: bigint;
+  readonly minerFee: bigint;
+  readonly network: NetworkContext;
 }
