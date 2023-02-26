ddd6849ea934f2eeeccf8a3a5f515669e56377f5
rsalakhov <r.salahov@spectrumlabs.fi>
2023-02-26 22:46:10 +0300

    update ammOrdersParser.ts #2

--- src/amm/common/parsers/ammOrdersParser.ts
+++ src/amm/common/parsers/ammOrdersParser.ts
@@ -26,13 +26,21 @@ interface ParserIn {
   parseRedeem(bx: ErgoBox): AmmOrderInfo | undefined
 }
 
-class T2TParserIn {
+abstract class T2TParserIn {
+  abstract getSwapPoolIdC (tree: any): Uint8Array
+
+  abstract getSwapOutIdC (tree: any): Uint8Array;
+
+  abstract getDepositPoolIdC (tree: any): Uint8Array;
+
+  abstract getRedeemPoolIdC (tree: any): Uint8Array;
+
   parseSwap(bx: ErgoBox): AmmOrderInfo | undefined {
     try {
       const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-      const poolIdC = tree.get_constant(14)?.to_byte_array()
+      const poolIdC = this.getSwapPoolIdC(tree);
       const poolId = poolIdC ? toHex(poolIdC) : undefined
-      const outIdC = tree.get_constant(2)?.to_byte_array()
+      const outIdC = this.getSwapOutIdC(tree);
       const outId = outIdC ? toHex(outIdC) : undefined
       const input = bx.assets[0]
       return poolId && outId
@@ -46,7 +54,7 @@ class T2TParserIn {
   parseDeposit(bx: ErgoBox): AmmOrderInfo | undefined {
     try {
       const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-      const poolIdC = tree.get_constant(13)?.to_byte_array()
+      const poolIdC = this.getDepositPoolIdC(tree)
       const poolId = poolIdC ? toHex(poolIdC) : undefined
       const inputX = bx.assets[0]
       const inputY = bx.assets[1]
@@ -61,7 +69,7 @@ class T2TParserIn {
   parseRedeem(bx: ErgoBox): AmmOrderInfo | undefined {
     try {
       const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-      const poolIdC = tree.get_constant(13)?.to_byte_array()
+      const poolIdC = this.getRedeemPoolIdC(tree);
       const poolId = poolIdC ? toHex(poolIdC) : undefined
       const inputLP = bx.assets[0]
       return poolId && inputLP ? {inLP: AssetAmount.fromToken(inputLP), poolId, type: "redeem"} : undefined
@@ -71,7 +79,53 @@ class T2TParserIn {
   }
 }
 
-class N2TParserIn {
+class NativeFeeT2tParserIn extends T2TParserIn {
+  getSwapPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(14)?.to_byte_array();
+  }
+
+  getSwapOutIdC(tree: any): Uint8Array {
+    return tree.get_constant(2)?.to_byte_array();
+  }
+
+  getDepositPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(13)?.to_byte_array();
+  }
+
+  getRedeemPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(13)?.to_byte_array();
+  }
+}
+
+class SpfFeeT2tParserIn extends T2TParserIn {
+  getSwapPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(18)?.to_byte_array();
+  }
+
+  getSwapOutIdC(tree: any): Uint8Array {
+    return tree.get_constant(1)?.to_byte_array();
+  }
+
+  getDepositPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(13)?.to_byte_array();
+  }
+
+  getRedeemPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(13)?.to_byte_array();
+  }
+}
+
+abstract class N2TParserIn {
+  abstract getSellSwapPoolIdC (tree: any): Uint8Array;
+
+  abstract getSellSwapOutIdC (tree: any): Uint8Array;
+
+  abstract getBuySwapPoolIdC (tree: any): Uint8Array;
+
+  abstract getDepositPoolIdC (tree: any): Uint8Array;
+
+  abstract getRedeemPoolIdC (tree: any): Uint8Array;
+
   parseSwap(bx: ErgoBox): AmmOrderInfo | undefined {
     try {
       return bx.assets.length > 0 ? this.parseBuySwap(bx) : this.parseSellSwap(bx)
@@ -82,9 +136,9 @@ class N2TParserIn {
 
   private parseSellSwap(bx: ErgoBox): AmmOrderInfo | undefined {
     const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-    const poolIdC = tree.get_constant(8)?.to_byte_array()
+    const poolIdC = this.getSellSwapPoolIdC(tree);
     const poolId = poolIdC ? toHex(poolIdC) : undefined
-    const outIdC = tree.get_constant(9)?.to_byte_array()
+    const outIdC = this.getSellSwapOutIdC(tree);
     const outId = outIdC ? toHex(outIdC) : undefined
     return poolId && outId
       ? {from: AssetAmount.native(bx.value), to: {id: outId}, poolId, type: "swap"}
@@ -93,7 +147,7 @@ class N2TParserIn {
 
   private parseBuySwap(bx: ErgoBox): AmmOrderInfo | undefined {
     const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-    const poolIdC = tree.get_constant(9)?.to_byte_array()
+    const poolIdC =  this.getBuySwapPoolIdC(tree);
     const poolId = poolIdC ? toHex(poolIdC) : undefined
     const input = bx.assets[0]
     return poolId
@@ -104,7 +158,7 @@ class N2TParserIn {
   parseDeposit(bx: ErgoBox): AmmOrderInfo | undefined {
     try {
       const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-      const poolIdC = tree.get_constant(12)?.to_byte_array()
+      const poolIdC = this.getDepositPoolIdC(tree);
       const poolId = poolIdC ? toHex(poolIdC) : undefined
       const inputY = bx.assets[0]
       return poolId && inputY
@@ -118,7 +172,7 @@ class N2TParserIn {
   parseRedeem(bx: ErgoBox): AmmOrderInfo | undefined {
     try {
       const tree = RustModule.SigmaRust.ErgoTree.from_base16_bytes(bx.ergoTree)
-      const poolIdC = tree.get_constant(11)?.to_byte_array()
+      const poolIdC = this.getRedeemPoolIdC(tree);
       const poolId = poolIdC ? toHex(poolIdC) : undefined
       const inputLP = bx.assets[0]
       return poolId && inputLP ? {inLP: AssetAmount.fromToken(inputLP), poolId, type: "redeem"} : undefined
@@ -128,25 +182,71 @@ class N2TParserIn {
   }
 }
 
-const t2tParser = new T2TParserIn()
-const n2tParser = new N2TParserIn()
+class NativeFeeN2TParserIn extends N2TParserIn {
+  getSellSwapPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(8)?.to_byte_array();
+  }
+
+  getSellSwapOutIdC(tree: any): Uint8Array {
+    return tree.get_constant(9)?.to_byte_array();
+  }
+
+  getBuySwapPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(9)?.to_byte_array()
+  }
+
+  getDepositPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(12)?.to_byte_array();
+  }
+
+  getRedeemPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(11)?.to_byte_array();
+  }
+}
+
+class SpfFeeN2TParserIn extends N2TParserIn {
+  getSellSwapPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(13)?.to_byte_array();
+  }
+
+  getSellSwapOutIdC(tree: any): Uint8Array {
+    return tree.get_constant(15)?.to_byte_array();
+  }
+
+  getBuySwapPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(11)?.to_byte_array()
+  }
+
+  getDepositPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(12)?.to_byte_array();
+  }
+
+  getRedeemPoolIdC(tree: any): Uint8Array {
+    return tree.get_constant(11)?.to_byte_array();
+  }
+}
+
+const nativeFeeT2tParser = new NativeFeeT2tParserIn();
+const spfFeeT2tParser = new SpfFeeT2tParserIn();
+const nativeFeeN2tParser = new NativeFeeN2TParserIn()
+const spfFeeN2TParserIn = new SpfFeeN2TParserIn();
 
 const AmmTemplates: [ErgoTreeTemplate, AmmOrderType, ParserIn][] = [
-  [NativeFeeT2T.SwapTemplate, "swap", t2tParser],
-  [NativeFeeT2T.DepositTemplate, "deposit", t2tParser],
-  [NativeFeeT2T.RedeemTemplate, "redeem", t2tParser],
-  [NativeFeeN2T.SwapSellTemplate, "swap", n2tParser],
-  [NativeFeeN2T.SwapBuyTemplate, "swap", n2tParser],
-  [NativeFeeN2T.DepositTemplate, "deposit", n2tParser],
-  [NativeFeeN2T.RedeemTemplate, "redeem", n2tParser],
+  [NativeFeeT2T.SwapTemplate, "swap", nativeFeeT2tParser],
+  [NativeFeeT2T.DepositTemplate, "deposit", nativeFeeT2tParser],
+  [NativeFeeT2T.RedeemTemplate, "redeem", nativeFeeT2tParser],
+  [NativeFeeN2T.SwapSellTemplate, "swap", nativeFeeN2tParser],
+  [NativeFeeN2T.SwapBuyTemplate, "swap", nativeFeeN2tParser],
+  [NativeFeeN2T.DepositTemplate, "deposit", nativeFeeN2tParser],
+  [NativeFeeN2T.RedeemTemplate, "redeem", nativeFeeN2tParser],
 
-  [SpfFeeT2T.SwapTemplate, "swap", t2tParser],
-  [SpfFeeT2T.DepositTemplate, "deposit", t2tParser],
-  [SpfFeeT2T.RedeemTemplate, "redeem", t2tParser],
-  [SpfFeeN2T.SwapSellTemplate, "swap", n2tParser],
-  [SpfFeeN2T.SwapBuyTemplate, "swap", n2tParser],
-  [SpfFeeN2T.DepositTemplate, "deposit", n2tParser],
-  [SpfFeeN2T.RedeemTemplate, "redeem", n2tParser]
+  [SpfFeeT2T.SwapTemplate, "swap", spfFeeT2tParser],
+  [SpfFeeT2T.DepositTemplate, "deposit", spfFeeT2tParser],
+  [SpfFeeT2T.RedeemTemplate, "redeem", spfFeeT2tParser],
+  [SpfFeeN2T.SwapSellTemplate, "swap", spfFeeN2TParserIn],
+  [SpfFeeN2T.SwapBuyTemplate, "swap", spfFeeN2TParserIn],
+  [SpfFeeN2T.DepositTemplate, "deposit", spfFeeN2TParserIn],
+  [SpfFeeN2T.RedeemTemplate, "redeem", spfFeeN2TParserIn]
 ]
 
 export class DefaultAmmOrdersParser implements AmmOrdersParser {
